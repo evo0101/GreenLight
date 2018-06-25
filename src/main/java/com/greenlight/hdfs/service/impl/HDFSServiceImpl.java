b712aad19f7553c1c86853237cbf73df9159cd60
Nathan Chandir <sabarinathanchandirasegarane@boomi.com>
2018-06-25 15:53:31 -0400

    Initial COmmit

---
+++ src/main/java/com/greenlight/hdfs/service/impl/HDFSServiceImpl.java
@@ -0,0 +1,12 @@
+package com.greenlight.hdfs.service.impl;
+
+
+public class HDFSServiceImpl {
+
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/test/java/com/greenlight/hdfs/service/impl/HDFSServiceTest.java
@@ -0,0 +1,20 @@
+package com.greenlight.hdfs.service.impl;
+import org.junit.Before;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+
+public class HDFSServiceTest {
+    @InjectMocks
+    HDFSServiceImpl xsltProcessorService;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    //Many more tests should be added but having a simple case
+    //More validations should be added as well. Keeping it simple here
+
+
+}
